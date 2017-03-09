e4d72d8f697e9f46bbd08c36a341c6d68885834d
Itamar Gronich <itamar.gronich@gizra.com>
2017-03-09 10:38:55 +0200

    Initial commit

---
+++ test/configs/custom-commands.js
@@ -0,0 +1,163 @@
+module.exports = (browser, capabilities, specs) => {
+
+  if (!browser && browser !== null && typeof browser === 'object') {
+    throw new TypeError("browserElement must be the WebdriverIo browser object.")
+  }
+
+  const
+    onDesktop = capabilities.browserName !== 'iPad',
+    onMobile  = !onDesktop;
+
+  // In BrowserStack - the bootup time is about 55 seconds, after that it
+  // launches a browser window and navigates to a local address that loads a
+  // page with the title "Let's browse!" we wait for this whole shenanigan to
+  // pass and then start the test.
+  if (onMobile) {
+    browser.waitUntil(
+      () =>
+        browser
+        // Find the let's browse title.
+          .elements('*')
+          .getText()
+          .includes('Let\'s browse!')
+      // Wait up to 130 seconds tp give browser stack enough time to load.
+      , 130000
+      , 'Didn\'t find \"let\'s browse\"'
+      , 1000
+    );
+  }
+
+  // On desktop Since the app isn't responsive at the moment - elements clip out
+  // of the view when width is under 1500.
+  if (onDesktop) {
+    browser.windowHandleSize({
+      width: 1500,
+      height: 900
+    });
+  }
+
+  /**
+   * Recursive function to ensure the correct text.
+   *
+   * This command is created in order to compensate the setValue() bug.
+   * The method (setValue) does not always set the correct value,
+   * sometimes it just misses some characters.
+   * This function sets each character at a time and recursively validates
+   * that the character is actually entered.
+   *
+   * @param {String} selector
+   *   The selector string to grab the element by.
+   * @param {String} text
+   *   The text that we want to set as a value.
+   */
+  browser.addCommand('setValueSafe', (selector, text) => {
+
+    // Get the ID of the selected elements WebElement JSON object.
+    const elementId = browser.element(selector).value.ELEMENT;
+    browser.waitUntil(() => browser.elementIdDisplayed(elementId));
+    /**
+     * Tackle the even weirder decision of WebDriver.io trim the spaces
+     * of every property value. Even the "value" property's value.
+     * I understand this for class or href properties but not for value.
+     * You can see it here : https://github.com/webdriverio/webdriverio/blob/acdd79bff797b295d2196b3616facc9005b6f17d/lib/webdriverio.js#L463
+     *
+     * @param {String} elementId
+     *   ID of a WebElement JSON object of the current element.
+     *
+     * @return {String}
+     *   The value of the `value` attribute.
+     */
+    const getActualText = elementId =>
+      browser
+        .elementIdAttribute(elementId, 'value')
+        .value;
+
+    let expected = '';
+
+    // Clear the input before entering new value.
+    browser.elementIdClear(elementId);
+
+    while (text) {
+      if (getActualText(elementId) == expected) {
+        const currentChar = text[0];
+        expected += currentChar;
+        text = text.slice(1);
+        browser.elementIdValue(elementId, currentChar);
+      }
+    }
+  });
+
+  /**
+   * Takes a user object and logs in as that user.
+   *
+   * @param {String} user - user to log in as.
+   */
+  browser.addCommand('login', (user) => {
+
+    // User must have username and password.
+    if (user) {
+      browser.goToUrl('/#login');
+      browser.setValueSafe('[name="username"]', user);
+      browser.setValueSafe('[name="password"]', user);
+      browser.submitForm('.form.narrow-form');
+      if (user == "john") {
+        // Teacher.
+        browser.waitForVisible('.ui.button.white');
+      } else {
+        browser.waitForVisible('.ui.large.fluid.primary.button');
+      }
+
+    } else {
+      throw new TypeError(
+        `====================================
+                 Error in login command:
+                 no username given.
+                 data given ${user}
+           ====================================`
+      );
+    }
+  });
+
+  /**
+   * Log out.
+   */
+  browser.addCommand('logout', () => {
+    browser.goToUrl('/#subjects');
+    browser.findAndClick('.user-menu > a');
+    browser.findAndClick('.item=Logout');
+    browser.waitForVisible('[name="username"]');
+    browser.waitForVisible('[name="password"]');
+  });
+
+  /**
+   * Find and click.
+   *
+   * @param {String} selector The css selector of the element to click.
+   */
+  browser.addCommand('findAndClick', (selector) => {
+    // Find and click the btn.
+    browser.waitForVisible(selector);
+    browser.click(selector);
+  });
+
+  /**
+   * Go to url only if not already there.
+   *
+   * Prevent reloading of the same page. MUST BE HERE.
+   * This prevents WDIO from filling out forms while trying to reload.
+   *
+   * @param {String} path - The path I.E '/#login'
+   */
+  browser.addCommand('goToUrl', (path) => {
+    const iAmNotThere =
+      !browser
+        .getUrl()
+        // Find the path which and escape any forward slashes.
+        .match(new RegExp(path, 'g'));
+
+    if (iAmNotThere) {
+      browser.url(path);
+    }
+  });
+
+};
---
+++ test/configs/wdio-browserstack-local.conf.js
@@ -0,0 +1,103 @@
+const browserstack = require('browserstack-local');
+
+exports.config = Object.assign(require('./wdio.conf').config, {
+  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+  //
+  // ============
+  // Capabilities
+  // ============
+  // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
+  // time. Depending on the number of capabilities, WebdriverIO launches several test
+  // sessions. Within your capabilities you can overwrite the spec and exclude options in
+  // order to group specific specs to a specific capability.
+  //
+  // First, you can define how many instances should be started at the same time. Let's
+  // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
+  // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
+  // files and you set maxInstances to 10, all spec files will get tested at the same time
+  // and 30 processes will get spawned. The property handles how many capabilities
+  // from the same test should run tests.
+  //
+  maxInstances: 5,
+  //
+  // If you have trouble getting all important capabilities together, check out the
+  // Sauce Labs platform configurator - a great tool to configure your capabilities:
+  // https://docs.saucelabs.com/reference/platforms-configurator
+  //
+  capabilities: [
+    {
+      'browserName': 'iPad',
+      'platform': 'MAC',
+      'device': 'iPad Air 2',
+      'deviceOrientation': 'landscape',
+      'project': 'Unio by harness',
+      'browserstack.debug': 'true',
+      'browserstack.local': 'true',
+      'name': 'Submitted by - ' + process.env.USER,
+      maxInstances: 7
+    }
+  ],
+  // Set a base URL in order to shorten url command calls. If your url parameter starts
+  // with "/", then the base url gets prepended.
+  baseUrl: 'http://localhost:3000',
+  //
+  // Initialize the browser instance with a WebdriverIO plugin. The object should have the
+  // plugin name as key and the desired plugin options as properties. Make sure you have
+  // the plugin installed before running any tests. The following plugins are currently
+  // available:
+  // WebdriverCSS: https://github.com/webdriverio/webdrivercss
+  // WebdriverRTC: https://github.com/webdriverio/webdriverrtc
+  // Browserevent: https://github.com/webdriverio/browserevent
+  // plugins: {
+  //     webdrivercss: {
+  //         screenshotRoot: 'my-shots',
+  //         failedComparisonsRoot: 'diffs',
+  //         misMatchTolerance: 0.05,
+  //         screenWidth: [320,480,640,1024]
+  //     },
+  //     webdriverrtc: {},
+  //     browserevent: {}
+  // },
+  //
+  // Test runner services
+  // Services take over a specific job you don't want to take care of. They enhance
+  // your test setup with almost no effort. Unlike plugins, they don't add new
+  // commands. Instead, they hook themselves up into the test process.
+  services: ['browserstack', 'selenium-standalone', 'phantomjs'],
+  //
+  // Test reporter for stdout.
+  // The only one supported by default is 'dot'
+  // see also: http://webdriver.io/guide/testrunner/reporters.html
+  reporters: ['spec'],
+
+  //
+  // =====
+  // Hooks
+  // =====
+  // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
+  // it and to build services around it. You can either apply a single function or an array of
+  // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
+  // resolved to continue.
+  //
+  // Gets executed once before all workers get launched.
+  // Code to start browserstack local before start of test
+  onPrepare: function (config, capabilities) {
+    console.log("Connecting local");
+    return new Promise(function(resolve, reject){
+      exports.bs_local = new browserstack.Local();
+      exports.bs_local.start({'key': exports.config.key }, function(error) {
+        if (error) return reject(error);
+        console.log('Connected. Now testing...');
+
+        resolve();
+      });
+    });
+  },
+  // Gets executed after all workers got shut down and the process is about to exit. It is not
+  // possible to defer the end of the process using a promise.
+  // Code to stop browserstack local after end of test
+  onComplete: function (capabilties, specs) {
+    exports.bs_local.stop(function() {});
+  }
+});
